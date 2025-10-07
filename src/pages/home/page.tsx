0215791ead52f3c81aba1ad56a87590b59e35f37
neominal.solutions <neominal.solutions@gmail.com>
2025-10-07 11:47:52 +0300

    Home Page Promise sample

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,5 +1,37 @@
+const promise = new Promise(function (myResolve, myReject) {
+	const x = 0;
+	console.log('x', x);
+
+	// some code (try to change x to 5)
+	setTimeout(() => {
+		if (x == 0) {
+			myResolve('OK');
+		} else {
+			myReject('Error');
+		}
+	}, 1000);
+});
+
+// JS default çalışma mantığı senkron kod blokları öncelikli olarak işlenir.
+// Daha sonra asenkron promise resolved yada rejected olduğunda JS tarafında kod ile ilgili işlem yapılır.
+// JS bu promiseler çözülene kadar kodları bekletmez. bloke etmez. 
+
 function HomePage() {
-    return <div>Welcome to the Home Page</div>;
+	const title = 'Home Page';
+
+	const loadData = async () => {
+		try {
+			const response = await promise;
+			console.log('response', response);
+		} catch (error) {
+			console.log('error', error);
+		}
+	};
+
+	loadData();
+	console.log('title', title);
+
+	return <div>Welcome to the Home Page</div>;
 }
 
 export default HomePage;
